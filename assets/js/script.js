f964b52028cbad3569f4c7376ce18f16780b1cfa
n-souta <vape1147@gmail.com>
2021-03-14 01:48:38 +0900

    swiper設置前

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,30 @@ $(function() {
     startPos = value;
   });
 });
+
+
+$(function() {
+  //swiper 768以下で起動
+  var swiper; 
+  $(window).on('load resize', function(){
+      var w = $(window).width();
+      if (w <= 768) {
+          if (swiper) {
+              return;
+          } else {
+              swiper = new Swiper('.swiper-container', {
+                  loop: true,
+                  pagination: {
+                      el: '.swiper-pagination',
+                      clickable: true,
+                  },
+              });
+          }
+      } else {
+          if (swiper) {
+              swiper.destroy();
+              swiper = undefined;
+          } 
+      } 
+  });
+}); 
